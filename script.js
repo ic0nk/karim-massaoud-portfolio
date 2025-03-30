75dae9ad22c90a2f60b5be5252116f0dadb9051a
ic0nk <wael.iconk@gmail.com>
2025-03-30 18:55:29 +0200

    ++25++

--- script.js
+++ script.js
@@ -106,3 +106,17 @@ function getSlideIndex(container) {
 function setSlideIndex(n, container) {
     container.setAttribute('data-slide-index', n);
 }
+
+// Smooth scrolling
+document.querySelectorAll('.cta-button').forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        
+        targetElement.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+});
