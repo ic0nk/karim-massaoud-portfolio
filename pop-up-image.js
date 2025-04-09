f5e7bfa9a59bda1ff2994569de01d91eec8f8cb7
ic0nk <wael.iconk@gmail.com>
2025-04-10 00:33:48 +0200

    img fix branding

--- pop-up-image.js
+++ pop-up-image.js
@@ -1,31 +1,58 @@
-let currentSlide = 0;
-const slides = document.querySelectorAll('.popupImage');
-const totalSlides = slides.length;
-function openPopup(index) {
-    currentSlide = index;
-    updateSlidePosition();
-    document.getElementById('imagePopup').style.display = 'flex';
+class PopupGallery {
+    constructor(popupId, slideSelector) {
+        this.currentSlide = 0;
+        this.popup = document.getElementById(popupId);
+        this.slides = this.popup.querySelectorAll(slideSelector);
+        this.totalSlides = this.slides.length;
+        this.updateSlidePosition();
+    }
+
+    openPopup(index) {
+        this.currentSlide = index;
+        this.updateSlidePosition();
+        this.popup.style.display = 'flex';
+    }
+
+    closePopup() {
+        this.popup.style.display = 'none';
+    }
+
+    updateSlidePosition() {
+        this.slides.forEach((slide, index) => {
+            slide.style.transform = `translateX(${(index - this.currentSlide) * 100}%)`;
+        });
+    }
+
+    changeSlide(direction) {
+        this.currentSlide += direction;
+        if (this.currentSlide < 0) this.currentSlide = this.totalSlides - 1;
+        if (this.currentSlide >= this.totalSlides) this.currentSlide = 0;
+        this.updateSlidePosition();
+    }
 }
 
-function closePopup() {
-    document.getElementById('imagePopup').style.display = 'none';
+// Initialize galleries
+const gallery1 = new PopupGallery('imagePopup1', '.popupImage');
+const gallery2 = new PopupGallery('imagePopup', '.popupImage');
+
+// Event handlers for gallery 1
+function openPopupGallery1(index) {
+    gallery1.openPopup(index);
+}
+function closePopupGallery1() {
+    gallery1.closePopup();
+}
+function changeSlideGallery1(direction) {
+    gallery1.changeSlide(direction);
 }
 
-function updateSlidePosition() {
-    slides.forEach((slide, index) => {
-        slide.style.transform = `translateX(${(index - currentSlide) * 100}%)`;
-    });
+// Event handlers for gallery 2
+function openPopupGallery2(index) {
+    gallery2.openPopup(index);
 }
-
-function changeSlide(direction) {
-    currentSlide += direction;
-    if (currentSlide < 0) currentSlide = totalSlides - 1;
-    if (currentSlide >= totalSlides) currentSlide = 0;
-    updateSlidePosition();
+function closePopupGallery2() {
+    gallery2.closePopup();
 }
-
-updateSlidePosition();
-
-document.querySelector('.popupOverlay').addEventListener('click', function(e) {
-    if (e.target === this) closePopup();
-});
+function changeSlideGallery2(direction) {
+    gallery2.changeSlide(direction);
+}
